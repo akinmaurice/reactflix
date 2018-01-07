91a3fa8bfdea47f607c1fedee3fe321b2b88e109
akinmaurice <akinmaurice@gmail.com>
2018-01-07 09:14:38 +0100

    Updated Fonts and Color Scheme

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import appTitle from '../helpers';
 
 const Jumbotron = props => (
   <section className="jumbotron text-center">
     <div className="container">
+      <div className="row text-center">
+        <div className="col-lg-12 homeFont">
+          {appTitle}
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-lg-12">
+          <p className="homeText">
+            VIEW LATEST MOVIES AND TV SHOWS
+          </p>
+          <br />
+        </div>
+      </div>
       <div className="row text-center">
         <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12" />
         <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
